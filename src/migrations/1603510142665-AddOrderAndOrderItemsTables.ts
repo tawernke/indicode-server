a1839237615ee18de578c56fa0ae1eab657afa55
Tim <t.a.wernke@gmail.com>
2020-10-24 12:41:50 -0700

    Correct order and orderItem migration

--- src/migrations/1603510142665-AddOrderAndOrderItemsTables.ts
+++ src/migrations/1603510142665-AddOrderAndOrderItemsTables.ts
@@ -1,15 +1,17 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class AddOrderItemsTable1603397960310 implements MigrationInterface {
-    name = 'AddOrderItemsTable1603397960310'
+export class AddOrderAndOrderItemsTables1603510142665 implements MigrationInterface {
+    name = 'AddOrderAndOrderItemsTables1603510142665'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "order_item" ("id" SERIAL NOT NULL, "productName" character varying NOT NULL, "quantity" integer NOT NULL, "price" double precision NOT NULL, "total" double precision NOT NULL, "orderId" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d01158fe15b1ead5c26fd7f4e90" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "order" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "email" character varying NOT NULL, "address" character varying NOT NULL, "address2" character varying NOT NULL, "city" character varying NOT NULL, "country" character varying NOT NULL, "zip" character varying NOT NULL, "total" double precision NOT NULL, "totalQuantity" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_1031171c13130102495201e3e20" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "order_item" ADD CONSTRAINT "FK_646bf9ece6f45dbe41c203e06e0" FOREIGN KEY ("orderId") REFERENCES "order"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "order_item" DROP CONSTRAINT "FK_646bf9ece6f45dbe41c203e06e0"`);
+        await queryRunner.query(`DROP TABLE "order"`);
         await queryRunner.query(`DROP TABLE "order_item"`);
     }
 
