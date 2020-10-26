698d43edd5607a889cb95ca80ddd34a8f0d968f9
Tim <t.a.wernke@gmail.com>
2020-10-25 19:38:06 -0700

    Update product inventory when order is made

--- src/resolvers/order.ts
+++ src/resolvers/order.ts
@@ -1,16 +1,34 @@
+import { Product } from "../entities/Product";
 import { Arg, Mutation, Resolver } from "type-graphql";
+import { getConnection } from "typeorm";
 import { Order } from "../entities/Order";
 import { OrderInput } from "./OrderInput";
 
 @Resolver()
 export class OrderResolver {
   @Mutation(() => Order)
-  async createOrder(
-    @Arg("orderInput") orderInput: OrderInput,
-  ): Promise<Order> {
+  async createOrder(@Arg("orderInput") orderInput: OrderInput): Promise<Order> {
     const order = await Order.create({
       ...orderInput,
     }).save();
+
+    const updateIds = order.orderItems.map((item) => item.id);
+    const productsToUpdate = await Product.findByIds(updateIds);
+
+    const updateInventoryQueries = productsToUpdate.map((product) => {
+      const soldQuantity =
+        order.orderItems.find(
+          (orderItem) => orderItem.productId === product.id
+        )?.quantity || 0;
+
+      return getConnection()
+        .createQueryBuilder()
+        .update(Product)
+        .set({ quantity: product.quantity - soldQuantity })
+        .where("id = :id", { id: product.id })
+        .execute();
+    });
+    await Promise.all(updateInventoryQueries);
 
     return order;
   }
